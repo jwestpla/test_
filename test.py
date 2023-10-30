b4b4a938ef2797425c1b9b4ba9e7d26c2eb98995
Joep Westplate <joepwestplate@MacBook-Pro-van-Joep-2.local>
2023-10-30 19:08:02 +0100

    First working application

--- test.py
+++ test.py
@@ -1,42 +1,27 @@
 import tkinter as tk
+from pathlib import Path
+from PIL import Image, ImageTk
 
-LARGE_FONT= ("Verdana", 12)
+def firstcommand():
+    root.quit()
 
-class SeaofBTCapp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-
-        container.pack(side="top", fill="both", expand = True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        frame = StartPage(container, self)
-
-        self.frames[StartPage] = frame
-
-        frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
-
-        
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+root = tk.Tk()
+root.geometry("800x600")
 
 
-app = SeaofBTCapp()
-app.mainloop()
+# Define working directory
+realwd = Path("Bilder/Romnes1.png")
+
+# Create a canvas for the image
+canvas = tk.Canvas(root, width=800, height=600)
+canvas.pack()
+
+# open image with TIL, resize it and convert to a tkinter object
+fileplacement = Image.open(realwd)
+newbackgroundimage = fileplacement.resize((800,600))
+pic = ImageTk.PhotoImage(newbackgroundimage)
+
+# Create the image and place it in the canvas
+canvas.create_image(0, 0, image=pic, anchor="nw")
+
+root.mainloop()
--- tkinter_main.py
+++ tkinter_main.py
@@ -1,14 +1,55 @@
 import tkinter as tk
+import sys
+import pathlib as Path
+from PIL import Image, ImageTk
 
-class mainframe(tk.Frame):
+class MenuBar(tk.Menu):
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Hello world")
-        label.pack()
-        label.bind("<1>", self.quit)
+        super().__init__(parent)
+        
+        file_menu = tk.Menu(self)
+        # Adds a menu option called "File"
+        self.add_cascade(label="File", menu=file_menu)
+        # Adds a button in the menu called "File"
+        file_menu.add_command(label="Exit", command=self.quit)
+        
+        new_menu = tk.Menu(self)
+        self.add_cascade(label="New", menu=new_menu)
+        new_menu.add_command(label="exit2", command=self.quit)
+        
+    def quit(self):
+        sys.exit()
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        # Standard geometry
+        self.title("First application")
+        self.geometry("800x600")
+        self.resizable(False, False)
+    
+        # Background image homescreen
+        # # Define working directory    
+        WdImage = Path.Path("Bilder/Romnes1.png")
+        # # Create the canvas for the image
+        canvas = tk.Canvas(self, width=800, height=600, highlightthickness=5, highlightbackground="black")
+        canvas.pack()
+        # # open image with TIL, resize it and convert to a tkinter object
+        fileplacement = Image.open(WdImage)
+        newbackgroundimage = fileplacement.resize((800,600))
+        pic = ImageTk.PhotoImage(newbackgroundimage)
+        # # Place the image in the canvas
+        canvas.create_image(0, 0, image=pic, anchor="nw")
+        
+        
+        # menubar configuration
+        menubar = MenuBar(self)
+        self.config(menu=menubar)
+        
+        self.mainloop()
+        
 
 
-root = tk.Tk()
-mainframe(root).pack()
-
-root.mainloop()
+a = App()
+    
